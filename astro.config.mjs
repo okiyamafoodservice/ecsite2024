1a43f1da920cf9fb8cf858be27a4b070dc40c130
okiyamafoodservice <naotookiyama@gmail.com>
2024-07-22 12:56:06 +0900

    ec

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // サイトのベースURLを設定
+  base: "/06_ecsite",
+
+  // ビルド出力ディレクトリを設定（デフォルトは 'dist'）
+  outDir: "./dist/06_ecsite",
+
+  // 必要に応じて他の設定を追加
 });
